e403c87d8352ee1c2711039c8dd71919f91ce32a
tfdace <acedoestefania@gmail.com>
2022-09-16 00:40:09 -0300

    se soluciona bug en filtro vacio

--- TPWinform_Vargas_Delgado/WindowsFormsApp_TP/frmArticulos.cs
+++ TPWinform_Vargas_Delgado/WindowsFormsApp_TP/frmArticulos.cs
@@ -185,18 +185,21 @@ namespace WindowsFormsApp_TP
                 string criterio = cboCriterio.SelectedItem.ToString();
                 string filtro = txtFiltro.Text;
                 dgvArticulo.DataSource = articulo.filtrar(campo, criterio, filtro);
+
                 int cantidad = 0;
                 lista = articulo.filtrar(campo, criterio, filtro);
                 lista.Capacity = cantidad;
+            
+
                 if (cantidad == 0)
-                {
+                
                     pbArticulos.Load("https://th.bing.com/th/id/R.5f8b98acd656c6d261777d035c50a112?rik=9e0Xk%2brW5WO0Yg&riu=http%3a%2f%2fimg2.wikia.nocookie.net%2f__cb20140518072131%2ftowerofsaviors%2fimages%2f4%2f47%2fPlaceholder.png&ehk=CZAAxtW4x95yvm5bFj%2fqN8pJu9M9F1JW8H5KVFRhKnk%3d&risl=&pid=ImgRaw&r=0");
-                }
+               
 
             }
-            catch (Exception )
+            catch
             {
-                throw;
+             
             }
         }
 
